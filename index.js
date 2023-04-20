dc14fac8b2b92b6db01b7aa6419e059f45209b7a
Colin McFadden <cmcfadden@gmail.com>
2023-04-20 13:17:07 -0500

    add gzip support, reduce size of output

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 #!/usr/bin/env node
+//This script is used to parse a tarball and output a gzipped JSON file with the index
+
 const process = require("process");
 const fs = require("fs");
 const tar = require("tar");
+const { Readable } = require('stream');
+const { createGzip } = require('zlib');
+
 
 const tarballPath = process.argv[2];
+const outputPath = process.argv[3];
 
 let totalOffset = 0;
 let result = {};
 let id = 0;
+
+
+async function compressAndWriteToFile(data, filename) {
+  // Create a readable stream from the string
+  const readable = Readable.from(data);
+  // Create a writable stream to hold the compressed data
+  const writable = fs.createWriteStream(filename);
+  // Create a compression transform stream with the gzip algorithm
+  const compressionStream = createGzip();
+  // Pipe the data through the compression stream and into the writable stream
+  await readable.pipe(compressionStream).pipe(writable);
+  console.log(`Compressed data written to ${filename}`);
+}
 
 const parser = new tar.Parse();
 
@@ -16,21 +35,14 @@ parser.on("entry", (entry) => {
   const BLOCK_SIZE = 512; // for MacOS
   const fileSize = entry.type === "File" ? entry.size : 0;
 
-  const startByte = fileSize > 0 ? totalOffset + headerSize : null;
-  const endByte = fileSize > 0 ? startByte + fileSize - 1 : null;
+  const s = fileSize > 0 ? totalOffset + headerSize : null;
+  const e = fileSize > 0 ? s + fileSize - 1 : null;
   const padding =
     fileSize % BLOCK_SIZE ? BLOCK_SIZE - (fileSize % BLOCK_SIZE) : 0;
 
   const fileInfo = {
-    id,
-    filePath: entry.path,
-    fileType: entry.type,
-    startByte,
-    endByte,
-    fileSize,
-    headerSize,
-    padding,
-    entry,
+    s,
+    e,
   };
 
   result[entry.path] = fileInfo;
@@ -40,8 +52,10 @@ parser.on("entry", (entry) => {
   entry.resume(); // Discard the file content and move to the next entry
 });
 
+
 parser.on("end", () => {
-  console.log(JSON.stringify(result, null, 2));
+  let content = JSON.stringify(result, null, 2);
+  let compressedContent = compressAndWriteToFile(content, outputPath)
 });
 
 fs.createReadStream(tarballPath).pipe(parser);
